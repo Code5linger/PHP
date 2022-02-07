f7ca1a58ff7f049cc5f7e2014d3654320a6cabc2
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-02-07 22:54:05 +0600

    Create index.php

---
+++ siteA/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+  echo "Hello World <hr/>";
+  //Single line Comment 
+  /*
+  Multi 
+  Line 
+  Comment 
+  */
+  $x = 10 /* + 5 */ + 20;
+  echo $x;
+  echo "<he />";
+
+  echo "echo 1 <hr/>";
+  echo "ECHO 2 <hr/>";
+  echo "EcHo 3 <hr/>";
+  ?>
+</body>
+
+</html>
